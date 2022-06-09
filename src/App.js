8f03b8e6517dd3524dd2a53aff02d8f97272a88d
Sajib Hossain <sajibhossain1185@gmail.com>
2022-06-09 17:20:12 +0600

    finished header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './components/global/Header'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+    </>
   )
 }
 
---
+++ src/components/global/Header.jsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { Article, CurrencyExchange, Home, LocalAtm } from '@mui/icons-material';
+
+const drawerWidth = 240;
+
+export default function PermanentDrawerLeft() {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <List>
+                    <Typography variant='h4' margin={1}>Saj Cryporto</Typography>
+                </List>
+                <Divider />
+                <List>
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <Home />
+                            </ListItemIcon>
+                            <ListItemText primary={"Home"} />
+                        </ListItemButton>
+                    </ListItem>
+
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <LocalAtm />
+                            </ListItemIcon>
+                            <ListItemText primary={"Cryptocurrencies"} />
+                        </ListItemButton>
+                    </ListItem>
+
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <CurrencyExchange />
+                            </ListItemIcon>
+                            <ListItemText primary={"Exchanges"} />
+                        </ListItemButton>
+                    </ListItem>
+
+                    <ListItem disablePadding>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <Article />
+                            </ListItemIcon>
+                            <ListItemText primary={"News"} />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </Box>
+    );
+}
